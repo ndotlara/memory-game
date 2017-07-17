e58b1c0122360aa1f1224994969ce22df48be335
ndotlara <natalialara.cd@gmail.com>
2017-07-17 06:58:27 -0400

    updated game logic with conditional statements and alerts

--- js/main.js
+++ js/main.js
@@ -1,7 +1,18 @@
 console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
+var cardOne = cards[3];
+cardsInPlay.push(cardOne);
+console.log('User flipped queen');
+
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log('User flipped king');
+
+if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+} alert('You found a match!');
+else if (cardsInPlay[0] !== cardsInPlay[1]) {
+  alert('Sorry, try again.');
+}
