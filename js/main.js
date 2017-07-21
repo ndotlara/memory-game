a0919884c833e7e28ce2f1472e217d611360ea08
ndotlara <natalialara.cd@gmail.com>
2017-07-21 01:49:05 -0400

    WIP - created game logic
    Updates from Assignment #9 included

--- js/main.js
+++ js/main.js
@@ -1,20 +1,21 @@
 var cards = ["queen", "queen", "king", "king"];
 
-var cardsInPlay = [
-  cardsInPlay.push(cardOne),
-  cardsInPlay.push(cardTwo)
-];
+var cardsInPlay = [];
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
-
-var cardOne = cards[2];
-var cardTwo = cards[3];
-
-if (cardsInPlay.length === 2 {
-  if (cardsInPlay[0]) === cardsInPlay[1]; {
-  alert("You found a match!");
-} else {
-  alert("Sorry, try again.")
-}
+var checkForMatch = function() {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");
+  } else {
+  console.log("Sorry, try again.");
+  }
 };
+
+var flipCard = function (cardId) {
+console.log("User flipped " + cards[cardId]);
+  cardsInPlay.push(cards[cardId]);
+  if (cardsInPlay.length === 2) {
+    checkForMatch();
+  }
+};
+  flipCard(0);
+  flipCard(2);
